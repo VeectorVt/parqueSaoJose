a3b2037fdaf94a7fff2e5c85f6a3ce8bbd19bec1
Victor César <victorocalheiros@outlook.com>
2025-05-20 22:28:10 -0300

    Ajuste no roteamento da venda

--- backend/src/venda/venda.routes.js
+++ backend/src/venda/venda.routes.js
@@ -6,11 +6,11 @@ const router = express.Router()
 const venda = require('./venda.controllers')
 // const auth = require('../middlewares/auth')
 
-router.get('/venda', venda.returnAllVendas)
+router.get('/venda', venda.getAllVendas)
 
-router.post('/register/venda', venda.registerNewVenda);
+router.post('/register/venda', venda.createVenda);
 
-router.put('/edit/venda/:id', venda.returnEditVenda);
+router.put('/edit/venda/:id', venda.updateVenda);
 
 router.delete('/delete/venda/:id', venda.deleteVenda);
 
