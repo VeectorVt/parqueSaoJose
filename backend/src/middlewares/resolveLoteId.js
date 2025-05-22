98e052b9543d915ca03a94021e9bb98411b04f37
Victor César <victorocalheiros@outlook.com>
2025-05-21 21:16:00 -0300

    resolveLoteId movido pro middleware

---
+++ backend/src/middlewares/resolveLoteId.js
@@ -0,0 +1,13 @@
+const Lote = require('../lotes/lotes.model');
+
+async function resolveLote(req, res, next) {
+  const { lote: loteNum, quadra } = req.body;
+  if (loteNum && quadra) {
+    const loteDoc = await Lote.findOne({ lote: loteNum, quadra });
+    if (!loteDoc) {
+      return res.status(400).json({ message: 'Lote não encontrado para número/quadra informados' });
+    }
+    req.body.lote = loteDoc._id;
+  }
+  next();
+}
